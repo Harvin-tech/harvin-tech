e626b55f6b274a08d446dd1a4d61b22a541bdb13
Saurav Bharti <sauravbharti38@gmail.com>
2024-12-15 00:07:42 +0530

    test file add

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import Test from '@/components/test/test';
-
-export default function TestPage() {
-  return (
-    <>
-      <Test />
-    </>
-  );
-}
--- src/components/test/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test = () => {
-  return <div>test Page</div>;
-};
-
-export default Test;
