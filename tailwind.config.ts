91c9aadc9c36fffddbe7dc1119f5a42a85a78850
Saurav Bharti <sauravbharti38@gmail.com>
2024-12-21 12:29:05 +0530

    minor fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,75 +6,97 @@ const config: Config = {
     './src/{pages,components,app}/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        'primary-dark': 'hsl(var(--clr-primary-dark))',
-        'primary-dark2': {
-          DEFAULT: 'hsl(var(--primary-dark2))',
-          foreground: 'hsl(var(--primary-dark2-foreground))',
-        },
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        'card-foreground': 'hsl(var(--card-foreground))',
-        content1: 'hsl(var(--content1))',
-        content2: 'hsl(var(--content2))',
-        content3: 'hsl(var(--content3))',
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        'popover-foreground': 'hsl(var(--popover-foreground))',
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        'secondary-foreground': 'hsl(var(--secondary-foreground))',
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        'muted-foreground': 'hsl(var(--muted-foreground))',
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        'accent-foreground': 'hsl(var(--accent-foreground))',
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        'destructive-foreground': 'hsl(var(--destructive-foreground))',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
-        },
-      },
-      spacing: {
-        navbarHeight: 'var(--navbarHeight)',
-        bothNavHeight: 'var(--bothNavHeight)',
-        secondaryNavbarHeight: 'var(--secondaryNavbarHeight)',
-      },
-      borderRadius: {
-        DEFAULT: 'var(--radius)',
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-dark': 'hsl(var(--clr-primary-dark))',
+  			'primary-dark2': {
+  				DEFAULT: 'hsl(var(--primary-dark2))',
+  				foreground: 'hsl(var(--primary-dark2-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			'card-foreground': 'hsl(var(--card-foreground))',
+  			content1: 'hsl(var(--content1))',
+  			content2: 'hsl(var(--content2))',
+  			content3: 'hsl(var(--content3))',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			'popover-foreground': 'hsl(var(--popover-foreground))',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			'secondary-foreground': 'hsl(var(--secondary-foreground))',
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			'muted-foreground': 'hsl(var(--muted-foreground))',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			'accent-foreground': 'hsl(var(--accent-foreground))',
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			'destructive-foreground': 'hsl(var(--destructive-foreground))',
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		spacing: {
+  			navbarHeight: 'var(--navbarHeight)',
+  			bothNavHeight: 'var(--bothNavHeight)',
+  			secondaryNavbarHeight: 'var(--secondaryNavbarHeight)'
+  		},
+  		borderRadius: {
+  			DEFAULT: 'var(--radius)',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [require('tailwindcss-animate')],
 };
