3db3f4b39ce7632a23c0043a0836a5bf668f43c2
Piyush Kumar <piyushkumar1116@gmail.com>
2025-03-29 10:00:49 +0530

    Add console log for environment variables in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
 import Home from '@/components/home';
 
 export default function page() {
+  console.log('hello', process.env);
   return (
     <>
       <Header />
