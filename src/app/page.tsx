66d95d3d968370a62bec453fd9b33fe4862a493d
Piyush Kumar <piyushkumar1116@gmail.com>
2025-03-29 10:11:13 +0530

    Remove console log for environment variables in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-'use client';
 import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
 import Home from '@/components/home';
 
 export default function page() {
-  console.log('hello', process.env);
   return (
     <>
       <Header />
