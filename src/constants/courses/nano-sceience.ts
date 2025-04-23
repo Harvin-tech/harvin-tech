7636d196508c8eb89f35e26642ca150c67ede050
Piyush Kumar <piyushkumar1116@gmail.com>
2025-04-24 01:18:05 +0530

    Update nanoScienceCourses to include additional PDFs and videos for enhanced course content

--- src/constants/courses/nano-sceience.ts
+++ src/constants/courses/nano-sceience.ts
@@ -3,14 +3,84 @@ const nanoScienceCourses = {
   slug: 'nanoscience',
   pdfs: [
     {
-      pdf_name: 'Nano Science',
+      pdf_name: 'PDF 1',
       pdf_url:
         'https://drive.google.com/file/d/1p527iCZA1-wf6ChglYydBqn0cKqSvDOc/view?usp=drive_link',
     },
     {
-      pdf_name: 'Nano Science',
+      pdf_name: 'PDF 2',
       pdf_url:
         'https://drive.google.com/file/d/1p527iCZA1-wf6ChglYydBqn0cKqSvDOc/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 3',
+      pdf_url:
+        'https://drive.google.com/file/d/1J8Fx3r_sM0xSz46szgzoaUTtC5zZVSyn/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 4',
+      pdf_url:
+        'https://drive.google.com/file/d/10w3_91kIqWWDkk35KEfA-TG4wL6_2Ccd/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 5',
+      pdf_url:
+        'https://drive.google.com/file/d/13JiUvjD89BIQrKu_ZNlkGOFJDQvfa-Mv/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 6',
+      pdf_url:
+        'https://drive.google.com/file/d/1BJXubnaaLT81TkduU7sS4smuo88w3MQf/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 7',
+      pdf_url:
+        'https://drive.google.com/file/d/1NlCV7rm-XgG3DatdY10tFWyHKm0B02x7/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 8',
+      pdf_url:
+        'https://drive.google.com/file/d/1xfsAMYMYxMeq7Pzvn0YbiXBxCkOtQ_5z/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 9',
+      pdf_url:
+        'https://drive.google.com/file/d/1L2m5qpdNMOK9HQQv5-lQnNnFfbBUwzQz/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 10',
+      pdf_url:
+        'https://drive.google.com/file/d/1SRAm8wp7Fqsr1wBQrr9sJWlLaK-WkI-x/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 11',
+      pdf_url:
+        'https://drive.google.com/file/d/1mvBf1u5ju8B8l0ijH06W28uUG6HbKNnx/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 12',
+      pdf_url:
+        'https://drive.google.com/file/d/1jD2ToF7uElZU2EXUdunYvBYsTMc2gWUy/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 13',
+      pdf_url:
+        'https://drive.google.com/file/d/1WuzvYXcCdrli3iYHVSE68us-5J8bd2Zr/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 14',
+      pdf_url:
+        'https://drive.google.com/file/d/1HgGK8UiKlRlI7JP0AABVPrbGHrMleEvt/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 15',
+      pdf_url:
+        'https://drive.google.com/file/d/1WEqQzCD_tZKNzimDkpboTI164U1iwyqn/view?usp=drive_link',
+    },
+    {
+      pdf_name: 'PDF 16',
+      pdf_url:
+        'https://drive.google.com/file/d/1jJSThpb3KO_WpUBRo2DOu7NCXXZ4qERg/view?usp=drive_link',
     },
   ],
   videos: [
@@ -23,6 +93,56 @@ const nanoScienceCourses = {
       video_name: 'Nano Science Class 2',
       video_url:
         'https://drive.google.com/file/d/1NcIYw4xqMJUl0DoZjY8dhBF4veO8I5Pm/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 3',
+      video_url:
+        'https://drive.google.com/file/d/13vAgEDA27J8qfW40OkuFgdic1b9ncvf2/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 4',
+      video_url:
+        'https://drive.google.com/file/d/1LdFvpy7dDWbBH_atHKS7P38gNUSyevSh/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 5',
+      video_url:
+        'https://drive.google.com/file/d/1cPKR8IdSqOByxmn8wRUCo6zEZjCNWI47/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 6',
+      video_url:
+        'https://drive.google.com/file/d/1yUchcksDpoWODqukqSuhLhOnRvvahGNd/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 7',
+      video_url:
+        'https://drive.google.com/file/d/1PnPOcMzk4Kaz5CrY5ZTCJv3D79i1XtCL/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 8',
+      video_url:
+        'https://drive.google.com/file/d/1_LrzW47EFmRESpaqTcwuHP3cwL55Gl2t/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 9',
+      video_url:
+        'https://drive.google.com/file/d/1gKsTOqe9t4lxV7yicVf_Q06UJ-XyjYQZ/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 10',
+      video_url:
+        'https://drive.google.com/file/d/1PaDzYDCO78Aes3VdD9hcH_QeFYelA-hL/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 11',
+      video_url:
+        'https://drive.google.com/file/d/1y2hH9uYe0x5ZB-sPStsElJPXFA573hjS/view?usp=drive_link',
+    },
+    {
+      video_name: 'Nano Science Class 12',
+      video_url:
+        'https://drive.google.com/file/d/1GEP3t7nIBF5o0q-DFKKBSBpDNqWdvaRq/view?usp=drive_link',
     },
   ],
 };
