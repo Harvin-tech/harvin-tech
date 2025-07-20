60bea96e217b139b60aa950de8b3f25f48d7db9a
Saurav Bharti <sauravbharti38@gmail.com>
2025-07-20 18:01:33 +0530

    Add new courses: Certified Digital Marketing, Medical Coding, and Pharmacovigilance
    - Created a new course constant for Certified Digital Marketing with associated PDFs and videos.
    - Added Medical Coding course constant with detailed video resources.
    - Introduced Pharmacovigilance course constant, including video links for each class.
    - Updated coursesMap to include new course slugs for Certified Digital Marketing, Medical Coding, and Pharmacovigilance.

---
+++ src/constants/courses/medical-coding.ts
@@ -0,0 +1,281 @@
+const MedicalCoding = {
+  course_name: 'Medical Coding',
+  slug: 'medical-coding',
+  pdfs: [
+    // {
+    //   pdf_name: 'PDF 1',
+    //   pdf_url:
+    //     'https://docs.google.com/presentation/d/13Mem5AJ-DR6K1cnnu7J6O8Zp6I0h1s08/edit?usp=sharing&ouid=104042567202356532187&rtpof=true&sd=true',
+    // },
+    // {
+    //   pdf_name: 'PDF 2',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1p527iCZA1-wf6ChglYydBqn0cKqSvDOc/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 3',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1J8Fx3r_sM0xSz46szgzoaUTtC5zZVSyn/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 4',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/10w3_91kIqWWDkk35KEfA-TG4wL6_2Ccd/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 5',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/13JiUvjD89BIQrKu_ZNlkGOFJDQvfa-Mv/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 6',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1BJXubnaaLT81TkduU7sS4smuo88w3MQf/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 7',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1NlCV7rm-XgG3DatdY10tFWyHKm0B02x7/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 8',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1xfsAMYMYxMeq7Pzvn0YbiXBxCkOtQ_5z/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 9',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1L2m5qpdNMOK9HQQv5-lQnNnFfbBUwzQz/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 10',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1SRAm8wp7Fqsr1wBQrr9sJWlLaK-WkI-x/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 11',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1mvBf1u5ju8B8l0ijH06W28uUG6HbKNnx/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 12',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1jD2ToF7uElZU2EXUdunYvBYsTMc2gWUy/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 13',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1WuzvYXcCdrli3iYHVSE68us-5J8bd2Zr/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 14',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1HgGK8UiKlRlI7JP0AABVPrbGHrMleEvt/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 15',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1WEqQzCD_tZKNzimDkpboTI164U1iwyqn/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 16',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1jJSThpb3KO_WpUBRo2DOu7NCXXZ4qERg/view?usp=drive_link',
+    // },
+  ],
+  videos: [
+    {
+      video_name: 'Medical Coding Class 0',
+      video_url:
+        'https://drive.google.com/file/d/1hpPcQ3zeGb3gb0Jc6e-C8rXg1RLlsRHt/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 1',
+      video_url:
+        'https://drive.google.com/file/d/19QM-oXHYs3ABWfknOL8L-qU7WG3O_A3E/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 2',
+      video_url:
+        'https://drive.google.com/file/d/1dxk6apzrkEslkPlf48eg1_QWkWC9reMy/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 3',
+      video_url:
+        'https://drive.google.com/file/d/1GFKBPyWtkAhRDpexhMDO2GHq6sKb2T_v/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 4',
+      video_url:
+        'https://drive.google.com/file/d/1lfBPGri5JyRXDUgPJ6Uj7rHq1NUIGXPb/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 5',
+      video_url:
+        'https://drive.google.com/file/d/1Yu5Tb7Wnx0i5UwSxFRMUXRxE1nWJzdPv/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 6',
+      video_url:
+        'https://drive.google.com/file/d/1xklcraV5NmuRX4lMyC-EKrB9HP6E95PH/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 7',
+      video_url:
+        'https://drive.google.com/file/d/10FdRiqiSIxe9xaCoZKIaf3tVG_GT05Ua/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 8',
+      video_url:
+        'https://drive.google.com/file/d/1NddJFHTLH_RbzJU58oqB6m4WzOLptcX8/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 9',
+      video_url:
+        'https://drive.google.com/file/d/1i0P24Lhg7iVeuIUpy3qyhqTLYbrm2z3A/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 10',
+      video_url:
+        'https://drive.google.com/file/d/1IqKdIbFcMv4rOXGZI4lxckuhb0UgMOm3/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 11',
+      video_url:
+        'https://drive.google.com/file/d/1vqlPCXErRfJDGL1_XUzXlWQMTN9KP89n/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 12',
+      video_url:
+        'https://drive.google.com/file/d/1dLthyfVDKZDvIxCvLz4NinhfJx2OE01D/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 13',
+      video_url:
+        'https://drive.google.com/file/d/1RPmLQ3ayrfz9pBal9kUzRxT_DfctW4AW/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 14',
+      video_url:
+        'https://drive.google.com/file/d/11XvBllYAi_cvuYiEvkdMHgGIICmz63VU/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 15',
+      video_url:
+        'https://drive.google.com/file/d/1TXpnVhmGIoeHICY9FEDyOix1LuIe1Mrr/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 16',
+      video_url:
+        'https://drive.google.com/file/d/1OlE3YTPGUZfmS9gBkvXBio20vlmUpjov/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 17',
+      video_url:
+        'https://drive.google.com/file/d/1ghSFhewIrMXADqzvb2NBe05khCKXJLW0/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 18',
+      video_url:
+        'https://drive.google.com/file/d/1ZnJP584ifG2Wasrk4WafL_zlfEplAhan/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 19',
+      video_url:
+        'https://drive.google.com/file/d/1QH7Jjmpv_D31Lx7Ef0NRPVINzbuyFLKa/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 20',
+      video_url:
+        'https://drive.google.com/file/d/1d-bFCyXCVlsuUDn712T1YSBPxJsSUR-d/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 21',
+      video_url:
+        'https://drive.google.com/file/d/1qAO9g6vGvhetrwDrxSHFHZNIODbxUzxW/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 22',
+      video_url:
+        'https://drive.google.com/file/d/1vBkrF9rGG-MsEyePGDtWxcix0RRXXQSV/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 23',
+      video_url:
+        'https://drive.google.com/file/d/17XUp2cQfo6orqLXLmiFRItIh6ERJMHGH/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 24',
+      video_url:
+        'https://drive.google.com/file/d/1yZBg-WJyklJ3SJ7z7Zoe3DVCQ8v0eeVy/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 25',
+      video_url:
+        'https://drive.google.com/file/d/1vtTqsBRLrBWjF1VotMl-jT__X7X8FFee/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 26',
+      video_url:
+        'https://drive.google.com/file/d/1a_bDsAZ2yHr16zu2gHnobNLGs09H_sVI/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 27',
+      video_url:
+        'https://drive.google.com/file/d/1MOXImdZqSV7Yx2Yg8jgQPCg9_LcVDL1i/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 28',
+      video_url:
+        'https://drive.google.com/file/d/1MOXImdZqSV7Yx2Yg8jgQPCg9_LcVDL1i/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 29',
+      video_url:
+        'https://drive.google.com/file/d/1glB5sYj1-YrPElInUMPXWhjiaHzYliG2/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 30',
+      video_url:
+        'https://drive.google.com/file/d/1L_ZK2myyzYtI9-Rr-3hmdntC3p0mkHMu/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 31',
+      video_url:
+        'https://drive.google.com/file/d/1wGYb8ne-Kc19u3Z_AdZYiYsGs6MD4izy/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 32',
+      video_url:
+        'https://drive.google.com/file/d/1Cn75lPFLXebu3vWktpk1c0Fg5X_UcwI7/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 33',
+      video_url:
+        'https://drive.google.com/file/d/1Cn75lPFLXebu3vWktpk1c0Fg5X_UcwI7/view?usp=drive_link',
+      //nahi hai link
+    },
+    {
+      video_name: 'Medical Coding Class 34',
+      video_url:
+        'https://drive.google.com/file/d/1Cn75lPFLXebu3vWktpk1c0Fg5X_UcwI7/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 35',
+      video_url:
+        'https://drive.google.com/file/d/1elK_DiHXiMTiQbLI3sr7EsUp-MCj-wQn/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 36',
+      video_url:
+        'https://drive.google.com/file/d/1M8N15pOD3AjJiHjrjYnxzrGXNX0r5iK1/view?usp=drive_link',
+    },
+    {
+      video_name: 'Medical Coding Class 37',
+      video_url:
+        'https://drive.google.com/file/d/1xseibvLqU4QB55F9Q6SMk33AuiWCTu_g/view?usp=drive_link',
+    },
+  ],
+};
+
+export { MedicalCoding };
---
+++ src/constants/courses/pharmaco.ts
@@ -0,0 +1,244 @@
+const Pharmacovogilance = {
+  course_name: 'Pharmacovogilance',
+  slug: 'pharmacovigilance',
+  pdfs: [
+    // {
+    //   pdf_name: 'PDF 1',
+    //   pdf_url:
+    //     'https://docs.google.com/presentation/d/13Mem5AJ-DR6K1cnnu7J6O8Zp6I0h1s08/edit?usp=sharing&ouid=104042567202356532187&rtpof=true&sd=true',
+    // },
+    // {
+    //   pdf_name: 'PDF 2',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1p527iCZA1-wf6ChglYydBqn0cKqSvDOc/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 3',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1J8Fx3r_sM0xSz46szgzoaUTtC5zZVSyn/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 4',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/10w3_91kIqWWDkk35KEfA-TG4wL6_2Ccd/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 5',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/13JiUvjD89BIQrKu_ZNlkGOFJDQvfa-Mv/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 6',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1BJXubnaaLT81TkduU7sS4smuo88w3MQf/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 7',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1NlCV7rm-XgG3DatdY10tFWyHKm0B02x7/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 8',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1xfsAMYMYxMeq7Pzvn0YbiXBxCkOtQ_5z/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 9',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1L2m5qpdNMOK9HQQv5-lQnNnFfbBUwzQz/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 10',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1SRAm8wp7Fqsr1wBQrr9sJWlLaK-WkI-x/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 11',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1mvBf1u5ju8B8l0ijH06W28uUG6HbKNnx/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 12',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1jD2ToF7uElZU2EXUdunYvBYsTMc2gWUy/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 13',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1WuzvYXcCdrli3iYHVSE68us-5J8bd2Zr/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 14',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1HgGK8UiKlRlI7JP0AABVPrbGHrMleEvt/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 15',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1WEqQzCD_tZKNzimDkpboTI164U1iwyqn/view?usp=drive_link',
+    // },
+    // {
+    //   pdf_name: 'PDF 16',
+    //   pdf_url:
+    //     'https://drive.google.com/file/d/1jJSThpb3KO_WpUBRo2DOu7NCXXZ4qERg/view?usp=drive_link',
+    // },
+  ],
+  videos: [
+    {
+      video_name: 'Pharmacovogilance Class 0',
+      video_url:
+        'https://drive.google.com/file/d/1hpPcQ3zeGb3gb0Jc6e-C8rXg1RLlsRHt/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 1',
+      video_url:
+        'https://drive.google.com/file/d/1fU2y6d6SWX1SztsaON3MIYlX4smJdPAJ/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 2',
+      video_url:
+        'https://drive.google.com/file/d/18BDXqbx-k-2e1dCZCEqxswdiZ-A-nYI8/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 3',
+      video_url:
+        'https://drive.google.com/file/d/19AbtMnaYU8hohn3e0Ol4zYSVB-NnhhQi/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 4',
+      video_url:
+        'https://drive.google.com/file/d/1NJSmB1Vt8H0vABmkOrdqgkcUKVoldxwf/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 5',
+      video_url:
+        'https://drive.google.com/file/d/1fuZCHEs9f6hkilz9RAgjS-ugJhimfCO9/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 6',
+      video_url:
+        'https://drive.google.com/file/d/1fuZCHEs9f6hkilz9RAgjS-ugJhimfCO9/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 7',
+      video_url:
+        'https://drive.google.com/file/d/17PlZke8mTnmxc9i357D4S3ycYD9DBqzu/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 8',
+      video_url:
+        'https://drive.google.com/file/d/17PlZke8mTnmxc9i357D4S3ycYD9DBqzu/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 9',
+      video_url:
+        'https://drive.google.com/file/d/1luEUOQye0wb2wy2XLlPF3tltkhxgCEZP/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 10',
+      video_url:
+        'https://drive.google.com/file/d/1kfZ7szciJgpCNVldA0Favh5kMjCpcOCV/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 11',
+      video_url:
+        'https://drive.google.com/file/d/15my087w165BUZQ_EWKmZ5yp1BhGL9hr8/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 12',
+      video_url:
+        'https://drive.google.com/file/d/1cNn2jfxtvscsB1DzO9ZOoKAF3cZqso42/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 13',
+      video_url:
+        'https://drive.google.com/file/d/1tP_y515m7q2N7mwPtwky7zUPyI_g2TXQ/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 14',
+      video_url:
+        'https://drive.google.com/open?id=10E-wtW-maRkL3xLljOO7M4hI7DaRwRia&usp=drive_copy',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 15',
+      video_url:
+        'https://drive.google.com/file/d/1Af03qCPpcjy9SnGM3h4vZFxF4cs6EEB1/view?usp=drive_link',
+    },
+    {
+      video_name: ' Pharmacovogilance Class 16',
+      video_url:
+        'https://drive.google.com/file/d/1IBvwfMS0n-QmpTXt3uALC3hc_InVKTHr/view?usp=drive_link',
+    },
+    {
+      video_name: ' Class 17',
+      video_url:
+        'https://drive.google.com/file/d/1BPW4zWWH-RwFE82inLNjaqeqSNf0ZV4F/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 18',
+      video_url:
+        'https://drive.google.com/file/d/1BPW4zWWH-RwFE82inLNjaqeqSNf0ZV4F/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 19',
+      video_url:
+        'https://drive.google.com/file/d/1HIuVY7-IDxyTvXMwj2P8GJHGOXdGFOSo/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 20',
+      video_url:
+        'https://drive.google.com/file/d/1D_9WkCN0NrM7LYa44ycDCoThCMAsqD0d/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 21',
+      video_url:
+        'https://drive.google.com/file/d/19ndyrlTGvib-04g4e_-zCa5-ai0jNpXm/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 22',
+      video_url:
+        'https://drive.google.com/file/d/1s4DuJ9m0Q7PTbVK-ZOh5v21x3FkxP5gZ/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 23',
+      video_url:
+        'https://drive.google.com/file/d/1Fs_5nrMuDa-8Ja9ZBUHgNEEi80mtd6Ct/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 24',
+      video_url:
+        'https://drive.google.com/file/d/1Fs_5nrMuDa-8Ja9ZBUHgNEEi80mtd6Ct/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 25',
+      video_url:
+        'https://drive.google.com/file/d/1Fs_5nrMuDa-8Ja9ZBUHgNEEi80mtd6Ct/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 26',
+      video_url:
+        'https://drive.google.com/file/d/1qyPTTl_NcZKKhvw4xhTRyi4N9uH4yK1M/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 27',
+      video_url:
+        'https://drive.google.com/file/d/1o8NNhHezOYf19ck-yA1ZbxlwHtMMko2n/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 28',
+      video_url:
+        'https://drive.google.com/file/d/16RkVlLEll4rfkh8hdSc8CUY8JNWC_n4k/view?usp=drive_link',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 29',
+      video_url: '',
+    },
+    {
+      video_name: 'Pharmacovogilance Class 30',
+      video_url:
+        'https://drive.google.com/file/d/1ZyzJZeTCVel3j58YY7gjG_jvNNtylkhM/view?usp=drive_link',
+    },
+  ],
+};
+
+export { Pharmacovogilance };
